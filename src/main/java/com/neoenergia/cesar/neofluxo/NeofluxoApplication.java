7856ce3ba7ddd8daa18bae61b3cdacdf295c66ee
Gabriel <gabriellucasknp@github.com>
2026-09-09 19:27:43 -0300

    Adicionando backend Spring Boot

---
+++ src/main/java/com/neoenergia/cesar/neofluxo/NeofluxoApplication.java
@@ -0,0 +1,13 @@
+package com.neoenergia.cesar.neofluxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeofluxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeofluxoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoenergia/cesar/neofluxo/NeofluxoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoenergia.cesar.neofluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeofluxoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
